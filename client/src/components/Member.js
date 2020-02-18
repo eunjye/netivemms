f53679eceb5379286596ffc946b4ff9ee86e21c6
eunjye <eunjh1220@naver.com>
2020-02-18 15:10:46 +0900

    db연동...

--- client/src/components/Member.js
+++ client/src/components/Member.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 
+// set phone number
+function setPhoneNum(_phone) {
+    const phone = [
+        _phone.substr(0, 3),
+        _phone.substr(3, 4),
+        _phone.substr(7, 4)
+    ];
+    return (phone[0] + '-' + phone[1] + '-' + phone[2]);
+}
 
+// set birthday
+function setBirth(_birth) {
+    const _date = _birth.split('T')[0]
+        , date = _date.split('-');
+    return (date[0] + '년 ' + date[1] + '월 ' + date[2] + '일');
+}
+
+// run
 class Member extends React.Component {
     state = {
-        birth: ''
+        birth: '',
+        phone: ''
     }
     componentDidMount() {
-        const _birth = this.props.birth
-            , _date = _birth.split('T')[0]
-            , date = _date.split('-');
-        this.setState({birth: date[0] + '년 ' + date[1] + '월 ' + date[2] + '일'});
+        this.setState({birth: setBirth(this.props.birth)});
+        this.setState({phone: setPhoneNum(this.props.phone)});
     }
 
     render() {
-        
         return (
             <tr>
                 <td>{this.props.number}</td>
                 <td>{this.props.name}</td>
                 <td>{this.props.gender}</td>
                 <td>{this.state.birth}</td>
-                <td>{this.props.phone}</td>
+                <td>{this.state.phone}</td>
                 <td>{this.props.address}</td>
             </tr>
         )
